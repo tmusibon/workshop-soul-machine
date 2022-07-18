f49a07eafd48652bd3328379930793edcbdfc6df
David Levy <lennox.davidlevy@gmail.com>
2022-07-18 13:44:23 -0400

    disabled CoS

--- token-server/src/app.ts
+++ token-server/src/app.ts
@@ -1,12 +1,11 @@
 import * as express from 'express';
 import { authRoutes } from './auth/auth-routes';
-import { audioRoutes } from './audio/audio-routes';
+//import { audioRoutes } from './audio/audio-routes';
 import { validateAppConfig } from './config/validate-config';
 import { appConfig } from './config/app-config';
 
 const cors = require('cors');
 class App {
-
   public app: express.Application;
 
   constructor() {
@@ -20,19 +19,18 @@ class App {
     // enable cors
     this.app.use(cors());
     // support application/json
-    this.app.use(express.urlencoded({extended: true}));
+    this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     // auth routing
     this.app.use('/auth', authRoutes);
     // audio recording routing
-    this.app.use('/audio', audioRoutes)
+    //this.app.use('/audio', audioRoutes)
 
     // health check
     this.app.get('/ping', (req, res) => {
       res.send('v1');
     });
   }
-
 }
 
 export default new App().app;
